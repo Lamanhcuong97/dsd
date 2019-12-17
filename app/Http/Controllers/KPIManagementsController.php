b491eba4ffa78cd669979f8017c31398a0802fcf
lamanhcuong <cuonglm@fabbi.io>
2019-12-17 13:27:33 +0700

    17-12

--- app/Http/Controllers/KPIManagementsController.php
+++ app/Http/Controllers/KPIManagementsController.php
@@ -48,9 +48,58 @@ class KPIManagementsController extends Controller
     
     public function detailKPIProject(Request $request, $id)
     {
-        $list_employee = json_decode(file_get_contents('https://pmptn13.herokuapp.com/users'));
+        $list_kpi_project = null;
+        $isEmpty = true;
+        if (isset($request->year)) {
+            $result = json_decode(@file_get_contents('http://microserviceteam2hust.000webhostapp.com/api/microservice/kpi/projects/'. $request->year .'?token=4614718215946240'));
+            if(!is_null($result)){
+                    $list_kpi_project = $result->data;
+                    $isEmpty = isset($result->data->result) ? true : false;
+            }
+        }elseif(isset($request->max_min)){
+            $result = json_decode(@file_get_contents('http://microserviceteam2hust.000webhostapp.com/api/microservice/project/kpi/' . $request->max_min));
+            if(!is_null($result)){
+                $list_kpi_project = $result->data;
+                $isEmpty = isset($result->data->result) ? true : false;
+            }
+        }else{
+            $result = json_decode(@file_get_contents('http://microserviceteam2hust.000webhostapp.com/api/microservice/kpi/project/' . $id . '?token=4614718215946240'));
+            if(!is_null($result)){
+                $list_kpi_project = $result->data;
+                $isEmpty = isset($result->data->result) ? true : false;
+                $kpi_standard = array_column((array)$list_kpi_project->standard, 'data');
+            }
+        }
 
-        return view('chitiet_KPIduan', compact('id'));
+       
+
+        return view('chitiet_KPIduan', compact('id', 'list_kpi_project', 'kpi_standard'));
+    }
+
+    public function update_criteria(Request $request, $id)
+    {
+        dd($request);
+        $data = json_encode($request->all());
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://microserviceteam2hust.000webhostapp.com/api/microservice/kpi/update/project');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        $headers = array();
+        $headers[] = 'Accept: application/json';
+        $headers[] = 'Content-Type: application/json';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+
+        return redirect()->route('chitiet_KPIduan', $id);
     }
 
     public function listKPIDepartments(Request $request)
